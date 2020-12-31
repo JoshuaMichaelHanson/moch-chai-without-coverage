79fc9d030f75e65178435dab456a9f4bad5a497a
JoshuaMichaelHanson <josh@hansonslogic.com>
2020-12-30 20:29:36 -0600

    feat(CS633) Moch-Chi without coverage Will be hosted on glitch and used for CS633 2021 Spring 1

---
+++ rock-paper-scissors.js
@@ -0,0 +1,21 @@
+// One possible way to solve rock-paper-scissors.
+
+const choices = {
+    rock: 0,
+    paper: 1,
+    scissors: 2
+  };
+  const outcomes = ["tie", "user", "computer"];
+  const numChoices = outcomes.length;
+  
+  function getRockPaperScissorsWinner(userChoice, computerChoice) {
+    console.log("getRockPaperScissorsWinner");
+    if (userChoice === "bomb") {
+      console.log('bomb');
+      return outcomes[1];
+    }
+    console.log('Not bomb');
+    return outcomes[
+      (choices[userChoice] - choices[computerChoice] + numChoices) % numChoices
+    ];
+  }
